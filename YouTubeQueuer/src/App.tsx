bd820f16e54d7c8f46bf79f26deb118946c8b055
Roy Gero <roy.gero.ii@gmail.com>
2024-02-27 11:17:05 -0500

    Adding the Header.
    Need to work on the styling a fair bit.

--- YouTubeQueuer/src/App.tsx
+++ YouTubeQueuer/src/App.tsx
@@ -1,16 +1,20 @@
+import Header from "./components/Header"
 import { ResponseList } from "./dummyData/data"
 import VideoCard from "./components/VideoCard"
 import { YoutubeResponse } from "./interfaces/YoutubeResponse"
 
 const App = () => {
   return (
-    <div>
-      {
-        ResponseList.map( (entry: YoutubeResponse, index: number) => (
-          <VideoCard data={entry} index={index}/>
-        ))
-      }
-    </div>
+    <>
+      <Header/>
+      <div className="grid justify-center items-center">
+        {
+          ResponseList.map( (entry: YoutubeResponse, index: number) => (
+            <VideoCard data={entry} index={index}/>
+          ))
+        }
+      </div>
+    </>
   )
 }
 
