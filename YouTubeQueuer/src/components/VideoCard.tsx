24cc1e4875d8a0aa3936d425c58a5eb20542d1f3
Roy Gero <roy.gero.ii@gmail.com>
2024-03-11 23:13:41 -0400

    Updating the colors a bit.

--- YouTubeQueuer/src/components/VideoCard.tsx
+++ YouTubeQueuer/src/components/VideoCard.tsx
@@ -13,7 +13,7 @@ const VideoCard: React.FC<Props> = ({data, isEven = false}) => {
     alert(`${data.id} would be added, if backend`)
   }
 
-  const background = `max-w-screen-sm rounded overflow-hidden shadow-lg grid items-center justify-items-center ${isEven ? "bg-gray-400" : ""}`
+  const background = `max-w-screen-sm rounded overflow-hidden shadow-lg grid items-center justify-items-center ${isEven ? "" : "text-gray-300 bg-gray-500"}`
   const submitButton = `hover:bg-gray-400 font-bold py-2 px-4 rounded inline-flex items-center ${isEven ? "bg-gray-300 text-gray-800" : "bg-gray-800 text-gray-300"}`
   const title: string = decode(data.snippet.title);
   const channelTitle: string = decode(data.snippet.channelTitle);
@@ -22,13 +22,13 @@ const VideoCard: React.FC<Props> = ({data, isEven = false}) => {
     <div className={background}>
       <img className="image-contain" src={data.snippet.thumbnails.max.url} alt={data.snippet.title}/>
       <div className="px-4 py-2">
-        <p className="text-gray-700 text-base font-bold">
+        <p className="text-base font-bold">
           {title}
         </p>
-        <p className="text-gray-700 text-base font-bold">
+        <p className="text-base font-bold">
           {channelTitle}
         </p>
-        <p className="text-gray-700 text-base">
+        <p className="text-base">
           {description}
         </p>
       </div>
