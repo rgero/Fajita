55a16fccd59517e32444438042f07535d61bd388
Roy Gero <roy.gero.ii@gmail.com>
2024-03-14 17:48:41 -0400

    Cleaning up the theme a little bit.

--- YouTubeQueuer/src/App.tsx
+++ YouTubeQueuer/src/App.tsx
@@ -1,13 +1,10 @@
 import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-import { ThemeProvider, createTheme } from "@mui/material/styles"
 
-import {CssBaseline} from "@mui/material"
 import { DarkModeProvider } from "./context/DarkModeContext";
 import MainPage from "./pages/MainPage";
 import PageNotFound from "./pages/PageNotFound";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Toaster } from "react-hot-toast";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -20,16 +17,16 @@ const queryClient = new QueryClient({
 const App = () => {
   return (
     <DarkModeProvider>
-        <QueryClientProvider client={queryClient}>
-          <ReactQueryDevtools initialIsOpen={false} />
-          <BrowserRouter>
-            <Routes>
-              <Route index element={<MainPage/>}/>
-              <Route path="queue" element={<PageNotFound/>}/>
-              <Route path='*' element={<Navigate replace to='/'/>}/>
-            </Routes>
-          </BrowserRouter>
-        </QueryClientProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <BrowserRouter>
+          <Routes>
+            <Route index element={<MainPage/>}/>
+            <Route path="queue" element={<PageNotFound/>}/>
+            <Route path='*' element={<Navigate replace to='/'/>}/>
+          </Routes>
+        </BrowserRouter>
+      </QueryClientProvider>
     </DarkModeProvider>
   )
 }
