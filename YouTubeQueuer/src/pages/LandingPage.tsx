7e74a325b6a6eaab850bdce9bac14e6c1dc6e1ea
Roy Gero <roy.gero.ii@gmail.com>
2024-03-19 19:54:04 -0400

    Adding the styling for the Landing Page.

--- YouTubeQueuer/src/pages/LandingPage.tsx
+++ YouTubeQueuer/src/pages/LandingPage.tsx
@@ -1,10 +1,21 @@
-import { Link } from "@mui/material"
+import { Button, Container, Grid, Typography } from "@mui/material"
 
 const LandingPage = () => {
   return (
-    <div>
-      <Link href={`${import.meta.env.VITE_BACKEND_URL}/login`}>Login to Thing</Link>
-    </div>
+    <Container disableGutters sx={{minWidth: "100%", backgroundColor: 'black'}}>
+      <Grid container sx={{height: "100vh"}} direction="column" justifyContent="center" alignItems="center">
+        <Grid item width="75%">
+          <img src="fajita.svg"/>
+        </Grid>
+        <Grid item>
+          <Typography>Do you trust me?</Typography>
+        </Grid>
+        <Grid>
+          <Button href={`${import.meta.env.VITE_BACKEND_URL}/login`}>Login to Fajita</Button>
+        </Grid>
+      </Grid>
+
+    </Container>
   )
 }
 
