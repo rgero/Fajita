538fe4be4a16f7e362364d1258b6f604fe0987aa
Roy Gero <roy.gero.ii@gmail.com>
2024-03-28 12:31:40 -0400

    Making it so you can't press play/pause too quickly.

--- YouTubeQueuer/src/components/Footer/DrawerOptions/PlayOption.tsx
+++ YouTubeQueuer/src/components/Footer/DrawerOptions/PlayOption.tsx
@@ -4,11 +4,13 @@ import { useCallback, useEffect, useState } from "react";
 import { Message } from "../../../interfaces/Message";
 import PauseCircleOutlineIcon from '@mui/icons-material/PauseCircleOutline';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import toast from "react-hot-toast";
 import { useSocket } from "../../../hooks/useWebSocket";
 
 const PlayOption = () => {
   const socket = useSocket();
   const [isPlaying, setPlaying] = useState<boolean|null>();
+  const [lastPress, setLastPress] = useState<Date|null>(new Date());
 
   const onMessage = useCallback( async (message:Message) => {
     setPlaying( () => message.player_state_int == 1)
@@ -22,6 +24,17 @@ const PlayOption = () => {
   }, [socket, onMessage]);
 
   const processClick = () => {
+    if (lastPress)
+    {
+      const delta:number = new Date().getTime() - lastPress.getTime();
+      if (delta < 1500)
+      {
+        toast.loading(`You're going too fast!`)
+        return;
+      }
+      
+    }
+    setLastPress (() => new Date());
     socket.emit("playPause");
   }
 
