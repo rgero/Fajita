67048f994333ae4c3a78060944dae9e0b425b5c7
Roy Gero <roy.gero.ii@gmail.com>
2024-04-02 00:16:16 -0400

    #4 - Using References is a smarter way to do this.

--- YouTubeQueuer/src/components/Queue/QueueList.tsx
+++ YouTubeQueuer/src/components/Queue/QueueList.tsx
@@ -1,8 +1,7 @@
 import { Box, Container, Divider, Typography } from "@mui/material";
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 
 import { Interaction } from "../../interfaces/Interaction";
-import { Message } from "../../interfaces/Message";
 import QueueCard from "./QueueCard";
 import Spinner from "../ui/Spinner";
 import { useSocket } from "../../hooks/useWebSocket";
@@ -12,42 +11,36 @@ const QueueList = () => {
   const socket = useSocket();
   const {isLoading, queueData, error, refetch} = useYouTubeQueue();
   const {current_index, interactions} = queueData;
-  const [currentIndex, setCurrentIndex] = useState<number>(current_index);
+  const [currentIndex, setCurrentIndex] = useState<number>(-1);
 
-  const scrollToCurrent = (target: number) => {
-    const yOffset = -80; 
-    if (!target) return;
-    const element = document.getElementById(target.toString());
-    
-    if (!element) return;
-    
-    const y = element.getBoundingClientRect().top + window.pageYOffset + yOffset;
-    window.scrollTo({top: y, behavior: 'smooth'});
-  }
+  const scrollToRef = useRef<HTMLElement>(null);
 
-  const onMessage = useCallback( async (message: Message) => {
-    setCurrentIndex( () => message.current_index)
-    scrollToCurrent(message.current_index);
-  }, []);
-
+    // This is for video Interactions
   const onNewVideo = useCallback( async () => {
     refetch();
   }, []);
-
+  
   useEffect(() => {
-    socket.on("player_status", onMessage);
     socket.on("new_video_interaction", onNewVideo);
-
     return () => {
-      socket.off("player_status", onMessage);
       socket.off("new_video_interaction", onNewVideo);
     };
-  }, [socket, onMessage]);
+  }, [socket]);
 
-  useEffect(()=> {
-    setCurrentIndex( () => current_index)
-    scrollToCurrent(current_index);
-  }, [queueData, current_index]);
+  // This is for setting the current Index
+  useEffect(()=> { 
+    setCurrentIndex( () => current_index);
+  }, [queueData, currentIndex]);
+
+
+  // This is for the scrolling.
+  useEffect( () => {
+    if( scrollToRef.current ) {
+      const yOffset = -80;
+      const y = scrollToRef?.current?.getBoundingClientRect().top + window.pageYOffset + yOffset;
+      window.scrollTo({top: y, behavior: 'smooth'});
+    }
+  }, [currentIndex]);
 
   if(isLoading) return (<Spinner/>)
   if (interactions.length == 0)
@@ -72,7 +65,12 @@ const QueueList = () => {
     <Container> 
       {
         interactions.map( (entry: Interaction, index: number) => (
-          <Box sx={{paddingBottom: {xs: 1}}} key={index} id={`${entry.index}`}>
+          <Box 
+            sx={{paddingBottom: {xs: 1}}} 
+            key={index} 
+            id={`${entry.index}`}
+            ref={ entry.index === currentIndex ? scrollToRef : null } 
+          >
             <QueueCard current={currentIndex} data={entry} key={index}/>
             <Divider/>
           </Box>
