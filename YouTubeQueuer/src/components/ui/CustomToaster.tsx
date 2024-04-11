5fa439806b72dc93f0726d319fec0766f8c5c7a8
Roy Gero <roy.gero.ii@gmail.com>
2024-04-10 21:59:51 -0400

    Creating a custom toaster class so I can try to keep the Dark Mode Context clean.

---
+++ YouTubeQueuer/src/components/ui/CustomToaster.tsx
@@ -0,0 +1,43 @@
+import toast, { Toaster, useToasterStore } from "react-hot-toast";
+
+import { useEffect } from "react";
+import { useTheme } from "@mui/material"
+
+const CustomToaster = () => {
+  const theme = useTheme();
+  const { toasts } = useToasterStore();
+
+  // Limit the number of Toasts displayed
+  const TOAST_LIMIT = 2;
+  useEffect(() => {
+    toasts
+      .filter((t) => t.visible) // Only consider visible toasts
+      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
+      .forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
+  }, [toasts]);
+
+  return (
+    <Toaster 
+      position="bottom-center"
+      gutter={12}
+      containerStyle={{margin: "8px", bottom:"17.5%"}}
+      toastOptions={
+        {
+          success: {duration: 3000}, 
+          error: {duration: 3000},
+          loading: {duration: 1500},
+          style: { 
+            fontSize: '16px',
+            fontWeight: 'bold',
+            maxWidth: '500px', 
+            padding: '16px 24px', 
+            backgroundColor: `${theme.palette.background.paper}`, 
+            color: `${theme.palette.text.primary}` 
+          }
+        }
+      }
+    />
+  )
+}
+
+export default CustomToaster
--- YouTubeQueuer/src/context/DarkModeContext.tsx
+++ YouTubeQueuer/src/context/DarkModeContext.tsx
@@ -1,7 +1,7 @@
 import { ThemeProvider, createTheme } from "@mui/material";
-import { createContext, useContext, useEffect, useMemo } from "react";
+import { createContext, useContext, useMemo } from "react";
 
-import toast, { Toaster, useToasterStore } from "react-hot-toast";
+import CustomToaster from "../components/ui/CustomToaster";
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
 const DarkModeContext = createContext({
@@ -10,20 +10,11 @@ const DarkModeContext = createContext({
 });
 
 const DarkModeProvider = ({ children }: {children: React.ReactNode}) => {
-  const { toasts } = useToasterStore();
+
   const [isDarkMode, setIsDarkMode] = useLocalStorageState(
     window.matchMedia("(prefers-color-scheme: dark)").matches.toString(),
     "isDarkMode"
   );
-
-  // Limit the number of Toasts displayed
-  const TOAST_LIMIT = 2;
-  useEffect(() => {
-    toasts
-      .filter((t) => t.visible) // Only consider visible toasts
-      .filter((_, i) => i >= TOAST_LIMIT) // Is toast index over limit?
-      .forEach((t) => toast.dismiss(t.id)); // Dismiss – Use toast.remove(t.id) for no exit animation
-  }, [toasts]);
 
   const mode = isDarkMode ? "dark" : "light";
   const theme = useMemo(
@@ -45,26 +36,7 @@ const DarkModeProvider = ({ children }: {children: React.ReactNode}) => {
       <ThemeProvider theme={theme}>
         {children}
       </ThemeProvider>
-      <Toaster 
-        position="bottom-center"
-        gutter={12}
-        containerStyle={{margin: "8px", bottom:"17.5%"}}
-        toastOptions={
-          {
-            success: {duration: 3000}, 
-            error: {duration: 3000},
-            loading: {duration: 1500},
-            style: { 
-              fontSize: '16px',
-              fontWeight: 'bold',
-              maxWidth: '500px', 
-              padding: '16px 24px', 
-              backgroundColor: `${theme.palette.background.paper}`, 
-              color: `${theme.palette.text.primary}` 
-            }
-          }
-        }
-      />
+      <CustomToaster/>
     </DarkModeContext.Provider>
   );
 }
