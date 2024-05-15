ac9cda6b29a93fe18da832680de02c5da6796542
Roy Gero <roy.gero.ii@gmail.com>
2024-05-14 23:22:14 -0400

    Clearing the Form once Feedback is submitted.

--- YouTubeQueuer/src/components/Feedback/FeedbackForm.tsx
+++ YouTubeQueuer/src/components/Feedback/FeedbackForm.tsx
@@ -1,4 +1,4 @@
-import { Button, Grid, TextField, Typography } from "@mui/material"
+import { Button, Container, Grid, TextField, Typography } from "@mui/material"
 
 import toast from "react-hot-toast";
 import { useCreateFeedback } from "./hooks/useCreateFeedback";
@@ -9,53 +9,58 @@ const FeedbackForm = () => {
   const [details, setDetails] = useState("");
   const {isAdding, addFeedback} = useCreateFeedback();
 
-  const handleSubmit = () => {
+  const handleSubmit = async () => {
     if (title == "")
     {
       toast.error("Suggestion field cannot be empty")
       return;
     }
-    addFeedback({title, description: details})
+    addFeedback({title, description: details}, {onSuccess: () => {
+      setDetails("");
+      setTitle("");
+    }});
   }
 
   return (
-    <Grid container direction="column" spacing={2}>
-      <Grid item>
-        <TextField
-          id="outlined-controlled"
-          label="Suggestion"
-          fullWidth
-          value={title}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setTitle(event.target.value);
-          }}
-          required
-          disabled={isAdding}
-        />
-      </Grid>
-      <Grid item>
-        <TextField
-          id="outlined-controlled"
-          label="Additional Details"
-          fullWidth
-          multiline
-          rows={5}
-          value={details}
-          onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
-            setDetails(event.target.value);
-          }}
-          disabled={isAdding}
-        />
-      </Grid>
-      <Grid container justifyContent={"space-evenly"} sx={{paddingTop: 4}}>
+    <Container>
+      <Grid container direction="column" spacing={2}>
         <Grid item>
-          <Button variant="contained" color="error"><Typography fontWeight={"bold"}>Clear</Typography></Button>
+          <TextField
+            id="outlined-controlled"
+            label="Suggestion"
+            fullWidth
+            value={title}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setTitle(event.target.value);
+            }}
+            required
+            disabled={isAdding}
+          />
         </Grid>
         <Grid item>
-          <Button variant="contained" color="success" onClick={handleSubmit}><Typography fontWeight={"bold"}>Submit</Typography></Button>
+          <TextField
+            id="outlined-controlled"
+            label="Additional Details"
+            fullWidth
+            multiline
+            rows={5}
+            value={details}
+            onChange={(event: React.ChangeEvent<HTMLInputElement>) => {
+              setDetails(event.target.value);
+            }}
+            disabled={isAdding}
+          />
+        </Grid>
+        <Grid container justifyContent={"space-evenly"} sx={{paddingTop: 4}}>
+          <Grid item>
+            <Button variant="contained" color="error"><Typography fontWeight={"bold"}>Clear</Typography></Button>
+          </Grid>
+          <Grid item>
+            <Button variant="contained" color="success" onClick={handleSubmit}><Typography fontWeight={"bold"}>Submit</Typography></Button>
+          </Grid>
         </Grid>
       </Grid>
-    </Grid>
+    </Container>
   )
 }
 
