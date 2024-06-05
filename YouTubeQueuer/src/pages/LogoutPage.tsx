8012aeacc2690a2bb1d8d1088616f7e3f4bc99a4
Roy Gero <roy.gero.ii@gmail.com>
2024-06-04 22:39:26 -0400

    Fixing the logout page having a white border.

--- YouTubeQueuer/src/pages/LogoutPage.tsx
+++ YouTubeQueuer/src/pages/LogoutPage.tsx
@@ -2,14 +2,15 @@ import { Container, Grid, IconButton, Typography } from "@mui/material";
 
 import { deleteAllCookies } from "../services/apiAuthentication";
 import { useEffect } from "react";
-import { useTheme } from '@mui/material/styles';
+import { Theme, useTheme } from '@mui/material/styles';
 
 const LogoutPage = () => {
   useEffect( ()=> {
     deleteAllCookies();
   },[])
 
-  const theme = useTheme();
+  const theme: Theme = useTheme();
+  document.body.style.backgroundColor = `${theme.palette.background.paper}`;
   return (
     <Container disableGutters sx={{minWidth: "100%", backgroundColor: `${theme.palette.background.paper}`, color: `${theme.palette.primary.light}`}}>
       <Grid container sx={{height: "100vh", paddingTop: "2rem"}} direction="column" alignItems="center">
