99d08a083e6662830fe76ebf935017c076d532a0
Roy Gero <roy.gero.ii@gmail.com>
2024-06-14 21:04:57 -0400

    Fixing a warning that I was getting from react-query.

--- YouTubeQueuer/src/components/Search/hooks/useSearchResults.ts
+++ YouTubeQueuer/src/components/Search/hooks/useSearchResults.ts
@@ -9,9 +9,18 @@ export interface SearchResponse {
   error: Error | null
 }
 
+const getQueryResults = async (searchTerm: string) => {
+  const results = await getSearchResults(searchTerm as string);
+  if (results)
+  {
+    return results;
+  }
+  return [];
+}
+
 export const useSearchResults = (): SearchResponse => {
   const [searchParams] = useSearchParams();
   const searchTerm:string|null = searchParams.get("search");
-  const {isLoading, data: searchResults = [], error} = useQuery({queryKey: ["searchResults", searchTerm], queryFn: ()=>getSearchResults(searchTerm as string), refetchInterval: false});
+  const {isLoading, data: searchResults = [], error} = useQuery({queryKey: ["searchResults", searchTerm], queryFn: ()=>getQueryResults(searchTerm as string), refetchInterval: false});
   return { isLoading, searchResults, error};
 }
