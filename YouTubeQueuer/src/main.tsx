4bf573bd6e6c5a8ab20a3ab2e5b4ccb75bf3d953
Roy Gero <roy.gero.ii@gmail.com>
2024-07-19 16:07:57 -0400

    #57 - Removing StrictMode so we only get one Toast Message upon connect.

--- YouTubeQueuer/src/main.tsx
+++ YouTubeQueuer/src/main.tsx
@@ -1,13 +1,10 @@
 import App from './App.tsx'
 import { ErrorBoundary } from 'react-error-boundary'
 import ErrorFallback from './components/ui/ErrorFallback.tsx'
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> window.location.replace("/")}>
-      <App />
-    </ErrorBoundary>
-  </React.StrictMode>
+  <ErrorBoundary FallbackComponent={ErrorFallback} onReset={()=> window.location.replace("/")}>
+    <App />
+  </ErrorBoundary>
 )
