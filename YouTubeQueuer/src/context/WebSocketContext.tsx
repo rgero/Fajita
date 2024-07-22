58e57ee693c047dbba0d92455d45365cf574c7e0
Roy Gero <roy.gero.ii@gmail.com>
2024-07-22 11:32:58 -0400

    After talking to Vince, we don't need the user ID in this anymore.

--- YouTubeQueuer/src/context/WebSocketContext.tsx
+++ YouTubeQueuer/src/context/WebSocketContext.tsx
@@ -1,20 +1,17 @@
 import { createContext, useContext } from "react";
 
 import io from 'socket.io-client';
-import { useUser } from "../components/authentication/hooks/useUser";
 
-const SocketContext = createContext(io());
-
-const SocketProvider = ({children}: {children: React.ReactNode}) => {
-  const {user} = useUser();
-  const socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
-    withCredentials: true,
-    extraHeaders: {
-        "Access-Control-Allow-Origin": `${import.meta.env.VITE_BACKEND_URL}`, // Match the origin allowed by Flask-SocketIO
-        "user_id": user ? user.id.toString() : ""
-    }
+const socket = io(`${import.meta.env.VITE_BACKEND_URL}`, {
+  withCredentials: true,
+  extraHeaders: {
+      "Access-Control-Allow-Origin": `${import.meta.env.VITE_BACKEND_URL}`, // Match the origin allowed by Flask-SocketIO
+  }
 });
 
+const SocketContext = createContext(socket);
+
+const SocketProvider = ({children}: {children: React.ReactNode}) => {
   return (
     <SocketContext.Provider value={socket}>
       {children}
