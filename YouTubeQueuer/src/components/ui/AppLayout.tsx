875b1d70752d8aa1ecde67d50cc3da405b1c60cf
Roy Gero <roy.gero.ii@gmail.com>
2024-08-29 10:27:03 -0400

    Restricting the size of the view on Mobile Devices
    Since the address bar takes up room on Chrome Mobile.

--- YouTubeQueuer/src/components/ui/AppLayout.tsx
+++ YouTubeQueuer/src/components/ui/AppLayout.tsx
@@ -2,12 +2,27 @@ import { Box, Container, CssBaseline } from "@mui/material";
 
 import Footer from "../Footer/FooterContainer";
 import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
 
 const AppLayout = () => {
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
+
+    setVh();
+    window.addEventListener("resize", setVh);
+
+    return () => {
+      window.removeEventListener("resize", setVh);
+    };
+  }, []);
+
   return (
     <>
       <CssBaseline />
-      <Box display="flex" flexDirection="column" height="100vh">
+      <Box display="flex" flexDirection="column" height="calc(var(--vh, 1vh) * 100)">
         <Box flexGrow={1} overflow="auto" display="flex" justifyContent="center" sx={{ mt: "6rem" }}>
           <Container disableGutters sx={{ width: { xs: "95%", md: "40%" } }}>
             <Outlet />
