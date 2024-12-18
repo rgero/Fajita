d5081d95ce0e5a8576a0629185a84d194a079bf2
Roy Gero <roy.gero.ii@gmail.com>
2024-12-18 16:12:11 -0500

    Cleaning up build errors

--- Nacho/src/context/SettingsContext.tsx
+++ Nacho/src/context/SettingsContext.tsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo } from "react";
+import { createContext, useContext } from "react";
 
 import { useLocalStorageState } from "../hooks/useLocalStorageState";
 
