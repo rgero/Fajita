5fced73375e45bd737a68547198a174495f224bc
Roy Gero <roy.gero.ii@gmail.com>
2025-09-01 21:48:21 -0400

    Enhancing the button to have the text color match the icon color.

--- Nacho/src/components/ui/Button.tsx
+++ Nacho/src/components/ui/Button.tsx
@@ -1,30 +1,41 @@
-import {Box, ButtonBase, Typography} from "@mui/material"
+import { Box, ButtonBase, Typography, useTheme } from "@mui/material"
 
+import { Palette } from "@mui/material/styles/createPalette"
 import React from "react"
 
 interface ButtonProps {
-  icon: React.ReactNode,
-  onClick: () => void,
-  title: string|null
+  icon: React.ReactNode
+  onClick: () => void
+  title: string | null
+  color?: keyof Palette | string
 }
 
-const Button: React.FC<ButtonProps> = ({icon, onClick, title}) => {
+const Button: React.FC<ButtonProps> = ({ icon, onClick, title, color }) => {
+  const theme = useTheme()
+
+  let resolvedColor: string = "inherit"
+
+  const paletteEntry = color ? (theme.palette as Palette)[color as keyof Palette] : undefined;
+  if (color && paletteEntry && typeof paletteEntry === "object" && "main" in paletteEntry) {
+    resolvedColor = (paletteEntry as { main: string }).main;
+  } else if (color) {
+    resolvedColor = color;
+  }
+
   return (
-    <ButtonBase 
+    <ButtonBase
       sx={{
         borderRadius: "40px",
-        paddingInline: "5px"
+        paddingInline: "5px",
+        color: resolvedColor,
       }}
       onClick={onClick}
     >
-      <Box
-        display="flex"
-        flexDirection="column"
-        alignItems="center"
-
-      >
+      <Box display="flex" flexDirection="column" alignItems="center">
         {icon}
-        <Typography variant="caption">{title}</Typography>
+        <Typography variant="caption" sx={{ color: resolvedColor }}>
+          {title}
+        </Typography>
       </Box>
     </ButtonBase>
   )
