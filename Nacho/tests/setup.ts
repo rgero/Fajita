82a479d2aa416de1c9fb3ed8ebc7febad1a844a2
Roy Gero <roy.gero.ii@gmail.com>
2025-10-14 14:58:05 -0400

    Removing the old test folder.
    I need to go through and start tests from scratch.

--- Nacho/tests/setup.ts
+++
@@ -1,9 +0,0 @@
-import '@testing-library/jest-dom/vitest'
-import '@testing-library/jest-dom';
-
-import { afterEach } from 'vitest'
-import { cleanup } from '@testing-library/react'
-
-afterEach(() => {
-  cleanup();
-})
