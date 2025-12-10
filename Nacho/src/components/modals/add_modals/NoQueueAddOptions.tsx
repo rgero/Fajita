c41ed53b7126fab51f7f0eb1189d2c541b3be0fc
Roy Gero <roy.gero.ii@gmail.com>
2025-12-09 20:00:50 -0500

    #154 - Adding a message and close button per feedback

--- Nacho/src/components/modals/add_modals/NoQueueAddOptions.tsx
+++ Nacho/src/components/modals/add_modals/NoQueueAddOptions.tsx
@@ -1,17 +1,19 @@
-import { Favorite, FavoriteBorder, Share, YouTube } from "@mui/icons-material";
+import { Close, Favorite, FavoriteBorder, Share, YouTube } from "@mui/icons-material";
+import { Grid, Typography } from "@mui/material";
 
 import Button from '../../ui/Button';
-import { Grid } from "@mui/material";
 import InfoSection from "../ui/InfoSection";
 import { OpenYouTubeURL } from "@utils/OpenYoutubeURL";
 import { copyVideoIDToClipboard } from "@utils/CopyToClipboard";
 import toast from "react-hot-toast";
+import { useModalContext } from "@context/modal/ModalContext";
 import { useSearchContext } from "@context/search/SearchContext";
 import { useSettings } from "@context/settings/SettingsContext";
 import { useStashContext } from "@context/stash/StashContext";
 
 const NoQueueAddOptions = () => {
   const {isInStash, addVideoToStash, deleteVideoFromStash} = useStashContext();
+  const {toggleAddToQueueModalOpen} = useModalContext();
   const {selectedResult} = useSearchContext();
   const {shareOptions} = useSettings();
   
@@ -34,8 +36,11 @@ const NoQueueAddOptions = () => {
 
   return (
     <InfoSection>
-      <Grid size={12} container justifyContent={"center"} sx={{paddingTop: 1}}>
-          <Grid container direction="row" spacing={1} alignItems="center">
+      <Grid size={12} container justifyContent={"center"}>
+          <Grid>
+            <Typography>No Queue Connected</Typography>
+          </Grid>
+          <Grid container size={12} justifyContent="space-evenly" direction="row" spacing={1} alignItems="center">
             {shareOptions.clipboard ? (
               <Grid>
                 <Button onClick={()=> copyVideoIDToClipboard(targetID)} icon={(<Share/>)} title="Copy"/>
@@ -48,6 +53,9 @@ const NoQueueAddOptions = () => {
             ) : null }
             <Grid>
               <Button onClick={processStash} icon={isInStash(targetID) ? <Favorite/> : <FavoriteBorder/>} title="Stash" color={isInStash(targetID) ? "error" : "default"}/>
+            </Grid>
+            <Grid>
+              <Button onClick={toggleAddToQueueModalOpen} icon={<Close/>} title="Close"/>
             </Grid>
           </Grid>
       </Grid>
